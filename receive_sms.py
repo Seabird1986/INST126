d2c586bb3d686fdeb41c82a929604540d97d9da5
Arthur Thomas <acthomasiv@gmail.com>
2017-12-01 14:45:36 -0500

    Running block to send text messages and recieve output which of zips
    We need to better format input removing their errors and format the output. Also we need to create if else statements for desired outputs

--- receive_sms.py
+++ receive_sms.py
@@ -1,4 +1,7 @@
-#testing environment
+import urllib.request as urlr
+import urllib.parse as url
+import urllib.error as urle
+import json
 import os
 from flask import Flask, request, redirect
 from twilio import twiml
@@ -16,15 +19,55 @@ def sms_reply():
         if len(body) != 5:
             r.message("Please try again, send your 5-digit zipcode")
     elif len(body) == 5:
-        r.message('Enter the information you would like to retrieve')
-        r.message('1. Polling Place. 2. State Senator 3. Election day')
+        s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+        zc = str(body)
+        s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+        serviceurl = s1+zc+s3
+        
+        address = serviceurl
+        print('Retrieving', address)
+        json_df = urlr.urlopen(address).read().decode('utf-8')
+        print('Retrieved', len(json_df), 'characters')
+        info = json.loads(json_df)
+        print('Entry count:', len(info))
 
-    elif body == "End" or body == "end":
-        r.message('You will no longer recieve texts from PollText')
+        Senate_reps = {}
+        
+        for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
+            if 'Senate' in item['name']:
+                Senate_reps = item['officialIndices']
+
+        for item in info['offices']:
+            if 'Representatives' in item['name']:
+                House_reps = item['officialIndices']
+        entries_count = 0
+        rep_number = 1
+        Senator_Info = {}
+        House_Info = {}
+        while entries_count < len(info['officials']):
+            for item in info['officials']:
+                if entries_count in Senate_reps:
+                    Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+                    #Senator_Info.update({'Senate Representative ' + str(rep_number) + ' address': item['address']})
+                    Senator_Info.update({'Senate Representative ' + str(rep_number) + ' phone': item['phones']})
+                    #Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+                    rep_number += 1
+                entries_count += 1
+
+        entries_count = 0
+        rep_number = 1
+        r.message(str(Senator_Info))                
     else:
         r.message('Sorry, that was not a valid input message.')
     return str(r)
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+#currently using my zip code as the testbed, will worry about user input zip codes later
+
+
+
+
