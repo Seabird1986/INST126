cb763a923dffef347dc3ffa11c871bee0f853a7d
Seabird <33431395+Seabird1986@users.noreply.github.com>
2017-12-02 17:57:21 -0500

    Test Format
    Playing around with the input/output format

---
+++ test_format.py
@@ -0,0 +1,148 @@
+import urllib.request as urlr
+import urllib.parse as url
+import urllib.error as urle
+import json
+import os
+
+global zip_code
+global info_reply
+zip_code = 0
+info_reply = 4
+
+
+begin = input("Type begin to start program: ")
+
+if begin.lower() == 'begin' or begin.lower() == 'begin ':
+    zip_code = input("Welcome to PollText! Reply with your 5-digit zip code to recieve phone number, email or website of your Senate Representative.")
+
+    while int(info_reply) > 3:
+        info_reply = input("Reply with the info you would like to receive: 1. Phone 2. Email 3. Website")
+        try:
+            int(info_reply) <= 3
+            int(info_reply) > 0
+        except:
+            print("Please enter a valid input.")
+else:
+    print('Sorry, that was not a valid input message.')
+    exit()
+
+
+serviceurl = 'https://www.googleapis.com/civicinfo/v2/representatives?address=' + str(zip_code) + '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+
+address = serviceurl
+print('Retrieving', address)
+json_df = urlr.urlopen(address).read().decode('utf-8')
+print('Retrieved', len(json_df), 'characters')
+info = json.loads(json_df)
+print('Entry count:', len(info))
+
+global Senate_reps
+global House_reps
+
+Senate_reps = {}
+House_reps = {}
+
+for item in info['offices']:  # create a list of the Official Indexes of Senate/House reps to pull info from
+    if 'Senate' in item['name']:
+        Senate_reps = item['officialIndices']
+
+for item in info['offices']:
+    if 'Representatives' in item['name']:
+         House_reps = item['officialIndices']
+
+global Senator_Info
+global House_Info
+
+def phone_reply(call_var):
+    Senator_Info = {}
+    House_Info = {}
+
+    entries_count = 0
+    rep_number = 1
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in Senate_reps:
+                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' phone': item['phones']})
+                rep_number += 1
+            entries_count += 1
+
+    entries_count = 0
+    rep_number = 1
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in House_reps:
+                House_Info.update({'House Representative ' + str(rep_number): item['name']})
+                House_Info.update({'House Representative ' + str(rep_number) + ' phone': item['phones']})
+                rep_number += 1
+            entries_count += 1
+
+    print(Senator_Info)
+    print(House_Info)
+
+
+def email_reply(call_var):
+
+    entries_count = 0
+    rep_number = 1
+    Senator_Info = {}
+    House_Info = {}
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in Senate_reps:
+                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+            rep_number += 1
+            entries_count += 1
+
+    entries_count = 0
+    rep_number = 1
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in House_reps:
+                House_Info.update({'House Representative ' + str(rep_number): item['name']})
+                House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
+            rep_number += 1
+            entries_count += 1
+
+    print(House_Info)
+    print(Senator_Info)
+
+def website_reply(call_var):
+    entries_count = 0
+    rep_number = 1
+    Senator_Info = {}
+    House_Info = {}
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in Senate_reps:
+                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+                rep_number += 1
+            entries_count += 1
+
+    entries_count = 0
+    rep_number = 1
+
+    while entries_count < len(info['officials']):
+        for item in info['officials']:
+            if entries_count in House_reps:
+                House_Info.update({'House Representative ' + str(rep_number): item['name']})
+                House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
+                rep_number += 1
+            entries_count += 1
+
+    print(House_Info)
+    print(Senator_Info)
+
+if info_reply == 1:
+    phone_reply(info_reply)
+elif info_reply == 2:
+    email_reply(info_reply)
+elif info_reply == 3:
+    website_reply(info_reply)
