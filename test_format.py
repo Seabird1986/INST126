139d612ba2f83174d1db10cc79300a92b4d29f15
Cam <cek1097@gmail.com>
2017-12-04 14:34:32 -0500

    Just reformated some stuff.
    I moved the functions up to the top of the code as per the stylization techniques discussed in class. I commented out some code just in case we need it later, and/or it was repeated. I also moved some code into an existing loop just to simplify the code.

--- test_format.py
+++ test_format.py
@@ -4,6 +4,28 @@ import urllib.error as urle
 import json
 import os
 
+def phone_reply(call_var):
+    Senator_Info = {}
+    House_Info = {}
+
+    entries_count = 0
+    rep_number = 1
+
+def email_reply(call_var):
+
+    entries_count = 0
+    rep_number = 1
+    Senator_Info = {}
+    House_Info = {}
+
+def website_reply(call_var):
+    entries_count = 0
+    rep_number = 1
+    Senator_Info = {}
+    House_Info = {}
+
+#Cam: Moved the functions up to the top.
+
 global zip_code
 global info_reply
 zip_code = 0
@@ -45,100 +67,81 @@ House_reps = {}
 for item in info['offices']:  # create a list of the Official Indexes of Senate/House reps to pull info from
     if 'Senate' in item['name']:
         Senate_reps = item['officialIndices']
-
-for item in info['offices']:
     if 'Representatives' in item['name']:
          House_reps = item['officialIndices']
 
-global Senator_Info
-global House_Info
+#for item in info['offices']: Cam: Only commented this out in case we need to put it back. But I took the 2 lines above and condensed it into one for loop.
+    
 
-def phone_reply(call_var):
-    Senator_Info = {}
-    House_Info = {}
+#global Senator_Info
+#global House_Info Cam: Just commented this out in case we need it. It ws repeated.
 
-    entries_count = 0
-    rep_number = 1
+entries_count = 0
+rep_number = 1
 
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in Senate_reps:
-                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
-                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' phone': item['phones']})
-                rep_number += 1
-            entries_count += 1
-
-    entries_count = 0
-    rep_number = 1
-
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in House_reps:
-                House_Info.update({'House Representative ' + str(rep_number): item['name']})
-                House_Info.update({'House Representative ' + str(rep_number) + ' phone': item['phones']})
-                rep_number += 1
-            entries_count += 1
-
-    print(Senator_Info)
-    print(House_Info)
-
-
-def email_reply(call_var):
-
-    entries_count = 0
-    rep_number = 1
-    Senator_Info = {}
-    House_Info = {}
-
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in Senate_reps:
-                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
-                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+while entries_count < len(info['officials']):
+    for item in info['officials']:
+        if entries_count in Senate_reps:
+            Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+            Senator_Info.update({'Senate Representative ' + str(rep_number) + ' phone': item['phones']})
             rep_number += 1
-            entries_count += 1
-
-    entries_count = 0
-    rep_number = 1
-
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in House_reps:
-                House_Info.update({'House Representative ' + str(rep_number): item['name']})
-                House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
+        entries_count += 1
+        if entries_count in House_reps:
+            House_Info.update({'House Representative ' + str(rep_number): item['name']})
+            House_Info.update({'House Representative ' + str(rep_number) + ' phone': item['phones']})
             rep_number += 1
-            entries_count += 1
+        entries_count += 1
 
+#while entries_count < len(info['officials']):
+#    for item in info['officials']: Cam: Just commented this out in case we need to put it back.
+        
+    print(Senator_Info)
+    print(House_Info)
+
+entries_count = 0
+rep_number = 1
+
+while entries_count < len(info['officials']):
+    for item in info['officials']:
+        if entries_count in Senate_reps:
+            Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+            Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+            rep_number += 1
+        entries_count += 1
+        if entries_count in House_reps:
+            House_Info.update({'House Representative ' + str(rep_number): item['name']})
+            House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
+            rep_number += 1
+        entries_count += 1
+
+#while entries_count < len(info['officials']):
+    #for item in info['officials']: Cam: Just commented this out in case we need it.
+            
     print(House_Info)
     print(Senator_Info)
 
-def website_reply(call_var):
-    entries_count = 0
-    rep_number = 1
-    Senator_Info = {}
-    House_Info = {}
-
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in Senate_reps:
-                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
-                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
-                rep_number += 1
-            entries_count += 1
-
-    entries_count = 0
-    rep_number = 1
-
-    while entries_count < len(info['officials']):
-        for item in info['officials']:
-            if entries_count in House_reps:
-                House_Info.update({'House Representative ' + str(rep_number): item['name']})
-                House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
-                rep_number += 1
-            entries_count += 1
-
-    print(House_Info)
-    print(Senator_Info)
+# Cam: Commented out this region just because it looked to be repeated. Might need it in the future.
+##    while entries_count < len(info['officials']):
+##        for item in info['officials']:
+##            if entries_count in Senate_reps:
+##                Senator_Info.update({'Senate Representative ' + str(rep_number): item['name']})
+##                Senator_Info.update({'Senate Representative ' + str(rep_number) + ' website': item['urls']})
+##                rep_number += 1
+##            entries_count += 1
+##
+##    entries_count = 0
+##    rep_number = 1
+##
+##    while entries_count < len(info['officials']):
+##        for item in info['officials']:
+##            if entries_count in House_reps:
+##                House_Info.update({'House Representative ' + str(rep_number): item['name']})
+##                House_Info.update({'House Representative ' + str(rep_number) + ' website': item['urls']})
+##                rep_number += 1
+##            entries_count += 1
+##
+##    print(House_Info)
+##    print(Senator_Info)
 
 if info_reply == 1:
     phone_reply(info_reply)
