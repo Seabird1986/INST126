962cd57f30d283467b3b674bf0f5548e2c79476b
Arthur Thomas <acthomasiv@gmail.com>
2017-12-05 00:53:57 -0500

    Updated Indents, which was causing alot of bugs.
    A break statement is needed, possible area for a while loop to be added, but without the break; sttatement everythign will print.

--- Twilio_format.py
+++ Twilio_format.py
@@ -14,56 +14,57 @@ app = Flask(__name__)
 
 def sms_reply():
     r = MessagingResponse()
-    body = request.values.get('Body', None) #for twilio 
+    body = request.values.get('Body', None) #for twilio
+    
     if body == 'SenPhone':
         r.message("Welcome to PollText! Reply with Zip for Senators Phone Num's.") #twil
 
-        try: 
-            if len(body) == 5:
-                url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-                z_c = str(body)
-                key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-                serviceurl = url+z_c+key
-                address = serviceurl
-                r.message(str(serviceurl))
-                r.message('Phone')
-            else:
-                r.message("SenPhone, SenWeb, SenEmail")
-        except ValueError:
-            pass               
+    try: 
+        if len(body) == 5:
+            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+            z_c = str(body)
+            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+            serviceurl = url+z_c+key
+            address = serviceurl
+            r.message(str(serviceurl))
+            r.message('Phone')
+        else:
+            r.message("SenPhone, SenWeb, SenEmail")
+    except ValueError:
+        pass               
 
     
     if body == 'SenWeb':
         r.message("Welcome to PollText! Reply with Zip for Senators Website.")# for twil
 
-        try:
-            if len(body) == 5:
-                url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-                z_c = str(body)
-                key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-                serviceurl = url+z_c+key
-                address = serviceurl
-                r.message(str(serviceurl))
-                r.message("Web")
-            else:
-                r.message("SenPhone,SenWeb, SenEmail")
-        except ValueError:
+    try:
+        if len(body) == 5:
+            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+            z_c = str(body)
+            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+            serviceurl = url+z_c+key
+            address = serviceurl
+            r.message(str(serviceurl))
+            r.message("Web")
+        else:
+            r.message("SenPhone,SenWeb, SenEmail")
+    except ValueError:
             pass
 
     
     if body == "SenEmail":
         r.message("Welcome to PollText! Reply with Zip for Senators Email's.")
-        try:
-            if len(body) == 5:
-                url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-                z_c = str(body)
-                key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-                serviceurl = url+z_c+key
-                address = serviceurl
-                r.message('Email')
+    try:
+        if len(body) == 5:
+            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+            z_c = str(body)
+            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+            serviceurl = url+z_c+key
+            address = serviceurl
+            r.message('Email')
 
-        except:
-            r.message('Sorry, that was not a valid input message.')
+    except:
+        r.message('Sorry, that was not a valid input message.')
 
     return str(r)
 
