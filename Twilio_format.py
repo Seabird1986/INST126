f395026b82ac7087ee5b064c32304523fe86a573
Arthur Thomas <acthomasiv@gmail.com>
2017-12-05 20:10:14 -0500

    It will not send the correct information.
    When the program runs, it simply return "phone" each and everytime, rather than giving web, or email. Indicating whent he api call is inserted, it wont run properly.

--- Twilio_format.py
+++ Twilio_format.py
@@ -14,58 +14,41 @@ app = Flask(__name__)
 
 def sms_reply():
     r = MessagingResponse()
-    body = request.values.get('Body', None) #for twilio
+    body = request.values.get('Body', None)
     
-    if body == 'SenPhone':
-        r.message("Welcome to PollText! Reply with Zip for Senators Phone Num's.") #twil
-
-    try: 
-        if len(body) == 5:
-            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-            z_c = str(body)
-            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-            serviceurl = url+z_c+key
-            address = serviceurl
-            r.message(str(serviceurl))
-            r.message('Phone')
-        else:
+    while True:
+        if body == 'SenPhone':
+            r.message("Welcome to PollText! Reply with Zip for Senators Phone Num's.")
+        try:
+            if len(body) == 5:
+                r.message('Phone')
+                break
+        except ValueError:
             r.message("SenPhone, SenWeb, SenEmail")
-    except ValueError:
-        pass               
-
-    
-    if body == 'SenWeb':
-        r.message("Welcome to PollText! Reply with Zip for Senators Website.")# for twil
-
-    try:
-        if len(body) == 5:
-            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-            z_c = str(body)
-            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-            serviceurl = url+z_c+key
-            address = serviceurl
-            r.message(str(serviceurl))
-            r.message("Web")
         else:
-            r.message("SenPhone,SenWeb, SenEmail")
-    except ValueError:
             pass
-
     
-    if body == "SenEmail":
-        r.message("Welcome to PollText! Reply with Zip for Senators Email's.")
-    try:
-        if len(body) == 5:
-            url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-            z_c = str(body)
-            key = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-            serviceurl = url+z_c+key
-            address = serviceurl
-            r.message('Email')
+        if body == 'SenWeb':
+            r.message("Welcome to PollText! Reply with Zip for Senators Website.")
+        try:
+            if len(body) == 5:
+                r.message("Web")
+                break
+        except ValueError:
+            r.message("SenPhone, SenWeb, SenEmail")
+        else:
+            break
 
-    except:
-        r.message('Sorry, that was not a valid input message.')
-
+        if body == "SenEmail":
+            r.message("Welcome to PollText! Reply with Zip for Senators Email's.")
+        try:
+            if len(body) == 5:
+                r.message('Email')
+                break
+        except ValueError:
+            r.message("SenPhone, SenWeb, SenEmail")
+        else:
+            break
     return str(r)
 
 if __name__ == "__main__":
