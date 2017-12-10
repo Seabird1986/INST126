48f9a6482021d4c3d390a903a21b4ea640852da7
Arthur Thomas <acthomasiv@gmail.com>
2017-12-09 19:33:11 -0500

    Added User Input Correct, and Data Validation!
    So it will now check if the zip is 5 digits again, and lower the users input, also remove any unwanted spaces. So far it looks nearly complete.

--- Twilio_format.py
+++ Twilio_format.py
@@ -8,7 +8,7 @@ from flask import Flask, request, redirect
 from twilio import twiml
 from twilio.twiml.messaging_response import MessagingResponse
 #   python /Users/cricket/Documents/GitHub/INST126/Twilio_format.py
-
+# we need to make an exception for DC cuz they have no data. 
 
 app = Flask(__name__)
 @app.route('/sms', methods=['GET', 'POST'])
@@ -17,86 +17,111 @@ def sms_reply():
     r = MessagingResponse()
     body = request.values.get('Body', None)
     if body.strip().lower() == "begin":
-       r.message("Welcome to SenText! Text 'Phone Number XXXXX', 'Websites XXXXX', or 'Address XXXXX' Replacing X's with 5-Digit ZipCode")        
-    elif len(body) == 18:
-        #Phone Number XXXXX' 18 char
-        body_zip = body[13:]
-        s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-        zc = str(body_zip)
-        s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-        serviceurl = s1+zc+s3
-        address = serviceurl
-        json_df = urlr.urlopen(address).read().decode('utf-8')
-        info = json.loads(json_df)
-        Senate_reps = {}
-        for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
-            if 'Senate' in item['name']:
-                Senate_reps = item['officialIndices']
-        entries_count = 0
-        rep_number = 1
-        Senator_Info = {}
-        while entries_count < len(info['officials']):
-            for item in info['officials']:
-                if entries_count in Senate_reps:
-                    Senator_Info.update({item['name']:item['phones']})
-                    rep_number += 1
-                entries_count += 1
-        entries_count = 0
-        rep_number = 1
+       r.message("Welcome to SenText! Text 'Phone XXXXX', 'Website XXXXX', or 'Address XXXXX' Replacing X's w/ 5-Digit ZipCode. (No DC Data Available)")        
+    elif body.lower().strip().startswith('phone'):
+        extract = re.findall(r'\d+', body)
+        extract = str(list(extract))
+        extract_num = extract[2:-2]
+        try:
+            if len(extract_num) == 5:      
+                s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+                zc = str(extract_num)
+                s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+                serviceurl = s1+zc+s3
+                address = serviceurl
+                json_df = urlr.urlopen(address).read().decode('utf-8')
+                info = json.loads(json_df)
+                Senate_reps = {}
+                for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
+                    if 'Senate' in item['name']:
+                        Senate_reps = item['officialIndices']
+                entries_count = 0
+                rep_number = 1
+                Senator_Info = {}
+                while entries_count < len(info['officials']):
+                    for item in info['officials']:
+                        if entries_count in Senate_reps:
+                            Senator_Info.update({item['name']:item['phones']})
+                            rep_number += 1
+                        entries_count += 1
+                entries_count = 0
+                rep_number = 1
+                r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
+            else:
+                print("Error")
+                r.message("Error: Zip must be 5-Digits")
+        except:
+            pass
         
-        r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
-    elif len(body) == 14:
-        #'Websites XXXXX' 14 char
-        body_zip = body[9:]
-        s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-        zc = str(body_zip)
-        s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-        serviceurl = s1+zc+s3
-        address = serviceurl
-        json_df = urlr.urlopen(address).read().decode('utf-8')
-        info = json.loads(json_df)
-        Senate_reps = {}
-        for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
-            if 'Senate' in item['name']:
-                Senate_reps = item['officialIndices']
-        entries_count = 0
-        rep_number = 1
-        Senator_Info = {}
-        while entries_count < len(info['officials']):
-            for item in info['officials']:
-                if entries_count in Senate_reps:
-                    Senator_Info.update({item['name']:item['urls']})
-                    rep_number += 1
-                entries_count += 1
-        entries_count = 0
-        rep_number = 1
-        r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
-    elif len(body) == 13:
-        #'Address XXXXX' 13 char
-        body_zip = body[8:]
-        s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
-        zc = str(body_zip)
-        s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
-        serviceurl = s1+zc+s3
-        address = serviceurl
-        json_df = urlr.urlopen(address).read().decode('utf-8')
-        info = json.loads(json_df)
-        Senate_reps = {}
-        for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
-            if 'Senate' in item['name']:
-                Senate_reps = item['officialIndices']
-        entries_count = 0
-        rep_number = 1
-        Senator_Info = {}
-        while entries_count < len(info['officials']):
-            for item in info['officials']:
-                if entries_count in Senate_reps:
-                    Senator_Info.update({item['name']:item['address']})
-                    rep_number += 1
-                entries_count += 1
-        entries_count = 0
-        rep_number = 1        
-        r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
+    elif body.strip().lower().startswith('web'):
+        extract = re.findall(r'\d+', body)
+        extract = str(list(extract))
+        extract_num = extract[2:-2]
+        try:
+            if len(extract_num) == 5: 
+                s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+                zc = str(extract_num)
+                s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+                serviceurl = s1+zc+s3
+                address = serviceurl
+                json_df = urlr.urlopen(address).read().decode('utf-8')
+                info = json.loads(json_df)
+                Senate_reps = {}
+                for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
+                    if 'Senate' in item['name']:
+                        Senate_reps = item['officialIndices']
+                entries_count = 0
+                rep_number = 1
+                Senator_Info = {}
+                while entries_count < len(info['officials']):
+                    for item in info['officials']:
+                        if entries_count in Senate_reps:
+                            Senator_Info.update({item['name']:item['urls']})
+                            rep_number += 1
+                        entries_count += 1
+                entries_count = 0
+                rep_number = 1
+                r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
+            else:
+                print("Error")
+                r.message("Error: Zip must be 5-Digits")
+        except:
+            pass
+                
+    elif body.strip().lower().startswith('ad'):
+        extract = re.findall(r'\d+', body)
+        extract = str(list(extract))
+        extract_num = extract[2:-2]
+        try:
+            if len(extract_num) == 5: 
+                s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+                zc = str(extract_num)
+                s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
+                serviceurl = s1+zc+s3
+                address = serviceurl
+                json_df = urlr.urlopen(address).read().decode('utf-8')
+                info = json.loads(json_df)
+                Senate_reps = {}
+                for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
+                    if 'Senate' in item['name']:
+                        Senate_reps = item['officialIndices']
+                entries_count = 0
+                rep_number = 1
+                Senator_Info = {}
+                while entries_count < len(info['officials']):
+                    for item in info['officials']:
+                        if entries_count in Senate_reps:
+                            Senator_Info.update({item['name']:item['address']})
+                            rep_number += 1
+                        entries_count += 1
+                entries_count = 0
+                rep_number = 1        
+                r.message(str(Senator_Info).replace("{","").replace("}","").replace("[","").replace("]","").replace("'",""))
+            else:
+                print("Error")
+                r.message("Error: Zip must be 5-Digits")
+        except:
+            pass                     
     else:
         r.message("Sorry, you need to check the format guidelines and try again.")
     return str(r)
