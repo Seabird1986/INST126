350963fb916affcc5d0b682c3b900dc60282973c
Arthur Thomas <acthomasiv@gmail.com>
2017-12-13 12:08:54 -0500

    Update from friday where we added our names for our contributions
    Sorry it took so long to upload this final version.  But this is the final program with comments.

--- Twilio_format.py
+++ Twilio_format.py
@@ -8,32 +8,31 @@ from flask import Flask, request, redirect
 from twilio import twiml
 from twilio.twiml.messaging_response import MessagingResponse
 #   python /Users/cricket/Documents/GitHub/INST126/Twilio_format.py
-# we need to make an exception for DC cuz they have no data. 
 
 app = Flask(__name__)
 @app.route('/sms', methods=['GET', 'POST'])
 
 def sms_reply():
     r = MessagingResponse()
-    body = request.values.get('Body', None) #this is the function that parses the messages sent.
+    body = request.values.get('Body', None) #this is the function that parses the messages sent.                       #Arthur Thomas
     if body.strip().lower() == "begin":
        r.message("Welcome to SenText! Text 'Phone XXXXX', 'Web XXXXX', or 'Address XXXXX' Replacing X's w/ 5-Digit ZipCode for Senator Info. (No DC Data Available)")        
-    elif body.lower().strip().startswith('phone'):
-        extract = re.findall(r'\d+', body)  #this function extracts all the integers from the string and puts them in a list
+    elif body.lower().strip().startswith('pho'):                                                                       #Jerry Dessy  
+        extract = re.findall(r'\d+', body)  #this function extracts all the integers from the string and puts them in a list 
         extract = str(list(extract))        #this converts the list to a string, and the next one strips brackets and quotes
         extract_num = extract[2:-2]
-        try:                                #this assures that the actually inputted a 5 digit zipcode. 
+        try:                                #this assures that the actually inputted a 5 digit zipcode.                #Cam Kelly
             if len(extract_num) == 5:      
-                s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='
+                s1 = 'https://www.googleapis.com/civicinfo/v2/representatives?address='                                #Dawit Ocbai
                 zc = str(extract_num)
                 s3 = '&key=AIzaSyBI4HcpedG-wMZPyVV8Sy7Q9Kal5i0EOy4'
                 serviceurl = s1+zc+s3
                 address = serviceurl
                 json_df = urlr.urlopen(address).read().decode('utf-8')
                 info = json.loads(json_df)
-                Senate_reps = {}
+                Senate_reps = {}                                                                                       #Christian Byrd
                 for item in info['offices']: #create a list of the Official Indexes of Senate/House reps to pull info from
-                    if 'Senate' in item['name']:
+                    if 'Senate' in item['name']:                                                
                         Senate_reps = item['officialIndices']
                 entries_count = 0
                 rep_number = 1
